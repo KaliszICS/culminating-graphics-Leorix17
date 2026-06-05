b4dedf47dc7725dd3eec26ed88eb000c741c5c7a
Leorix17 <343430617@tdsb.ca>
2026-06-05 15:44:44 +0000

    gg

--- HelloFX.java
+++ HelloFX.java
@@ -1,20 +1,29 @@
 import javafx.application.Application;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root,Color.WHITE);
+        Scene scene = new Scene(root,Color.SKYBLUE);
 
         stage.setTitle("Leledash");
 
@@ -25,6 +34,12 @@ public class HelloFX extends Application {
 
         stage.setFullScreen(true);
 
+        Text title = new Text();
+        title.setText("Leledash");
+        title.setX(500);
+        title.setY(500);
+        root.getChildren().add(title);
+        title.setFont(Font.font("Urban", 50));
 
     }
 
