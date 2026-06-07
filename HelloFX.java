3c609aa6d83d90ef1882702025e006eb1805f005
Leorix17 <343430617@tdsb.ca>
2026-06-07 05:08:33 +0000

    gg

--- HelloFX.java
+++ HelloFX.java
@@ -6,40 +6,60 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.animation.AnimationTimer;
 
 
 public class HelloFX extends Application {
+    private Scene menuScreen(Stage stage) {
+        VBox menuLay = new VBox(20);
+        Scene scene = new Scene(menuLay,800,500);
+        Text title = new Text("Leledash");
+        menuLay.setAlignment(Pos.CENTER);
+        
+        Button startBtn = new Button("Start Game");
+        startBtn.setOnAction(e -> stage.setScene(gameScreen(stage)));
+        Button instructionsBtn = new Button("How to play");
+        Button leaderboardBtn = new Button("Leaderboard");
+        menuLay.getChildren().addAll(title,startBtn,instructionsBtn,leaderboardBtn);
+        title.setFont(Font.font("Arial", 50));
+        return scene;
+    }
+
+    private Scene gameScreen(Stage stage) {
+        AnimationTimer gameLoop = new AnimationTimer() {
+
+        }
+        return scene;
+    }   
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root,Color.SKYBLUE);
 
         stage.setTitle("Leledash");
 
         Image icon = new Image("Drawing.png");
         stage.getIcons().add(icon);
-        stage.setScene(scene);
+
+        Scene menu = menuScreen(stage);
+        stage.setScene(menu);
         stage.show();
 
-        stage.setFullScreen(true);
+       
 
-        Text title = new Text();
-        title.setText("Leledash");
-        title.setX(500);
-        title.setY(500);
-        root.getChildren().add(title);
-        title.setFont(Font.font("Urban", 50));
+        
+
+
 
     }
 
