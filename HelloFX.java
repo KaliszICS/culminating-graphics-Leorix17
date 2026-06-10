aa45694ba33f88410aa64d040d9ed406329622d3
Leorix17 <343430617@tdsb.ca>
2026-06-10 13:44:11 +0000

    gg

--- HelloFX.java
+++ HelloFX.java
@@ -1,3 +1,4 @@
+
 /* 
     File: Leledash
     Author: Leo
@@ -27,36 +28,48 @@ import javafx.geometry.Pos;
 import javafx.animation.AnimationTimer;
 import java.util.ArrayList;
 
-
 public class HelloFX extends Application {
 
     AnimationTimer gameLoop;
-    //menu screen method
+
+    // menu screen method
     private Scene menuScreen(Stage stage) {
         VBox menuLay = new VBox(20);
-        Scene scene = new Scene(menuLay,800,500);
+        Scene scene = new Scene(menuLay, 800, 500);
         Text title = new Text("Leledash");
         menuLay.setAlignment(Pos.CENTER);
-        
-        //add buttons on the menu screen
+
+        // add buttons on the menu screen
         Button startBtn = new Button("Start Game");
-        //switch to game screen when start button is pressed
+        // switch to game screen when start button is pressed
         startBtn.setOnAction(e -> stage.setScene(gameScreen(stage)));
         Button instructionsBtn = new Button("How to play");
         Button leaderboardBtn = new Button("Leaderboard");
-        menuLay.getChildren().addAll(title,startBtn,instructionsBtn,leaderboardBtn); //adds all nodes to the scene
+        menuLay.getChildren().addAll(title, startBtn, instructionsBtn, leaderboardBtn); // adds all nodes to the scene
         title.setFont(Font.font("Arial", 50));
         return scene;
     }
 
     private Scene deathScreen(Stage stage) {
         Pane deathLay = new Pane();
-        Scene scene = new Scene(deathLay,800,500);
+        Scene scene = new Scene(deathLay, 800, 500);
         Button retryBtn = new Button("Retry");
         deathLay.getChildren().add(retryBtn);
         retryBtn.setOnAction(e -> stage.setScene(gameScreen(stage)));
         return scene;
-    }   
+    }
+
+    private Scene winScreen(Stage stage) {
+        VBox winLay = new VBox(20);
+        Scene scene = new Scene(winLay, 1080, 1920);
+        Button startBtn = new Button("Start Game");
+
+        Text winMsg = new Text("LEVEL COMPLETE");
+        Text attempts = new Text();
+        winLay.setAlignment(Pos.CENTER);
+    }
+
+
     private Scene gameScreen(Stage stage) { //game screen method
         Pane gameLay = new Pane(); //pane root for easier positioning of elements 
         Scene scene = new Scene(gameLay,800,500);
@@ -101,6 +114,9 @@ public class HelloFX extends Application {
         Rectangle spike9 = new Rectangle(100,40,Color.RED);
         Rectangle spike10 = new Rectangle(1600,40,Color.RED);
         Rectangle spike11 = new Rectangle(40,40,Color.RED);
+        Rectangle spike12 = new Rectangle(115,40,Color.RED);
+        Rectangle spike13 = new Rectangle(100,40,Color.RED);
+        Rectangle spike14 = new Rectangle(100,40,Color.RED);
         Rectangle plat1 = new Rectangle(200,20,Color.GREEN);
         Rectangle plat2 = new Rectangle(200,20,Color.GREEN);
         Rectangle plat3 = new Rectangle(100,20,Color.GREEN);
@@ -120,6 +136,9 @@ public class HelloFX extends Application {
         spike9.setX(4100); 
         spike10.setX(4600);        
         spike11.setX(6200);   
+        spike12.setX(6600);   
+        spike13.setX(7000);   
+        spike14.setX(7500);   
 
         spike1.setY(360);
         spike2.setY(360);
@@ -131,7 +150,10 @@ public class HelloFX extends Application {
         spike8.setY(360);
         spike9.setY(360);
         spike10.setY(360);
-        spike11.setY(140);  
+        spike11.setY(140);
+        spike12.setY(360);  
+        spike13.setY(360);  
+        spike14.setY(360);  
 
         plat1.setX(2500);
         plat2.setX(2800);       
@@ -161,6 +183,9 @@ public class HelloFX extends Application {
         spikes.add(spike9);
         spikes.add(spike10);
         spikes.add(spike11);
+        spikes.add(spike12);
+        spikes.add(spike13);
+        spikes.add(spike14);
         platforms.add(plat1);
         platforms.add(plat2);
         platforms.add(plat3);
@@ -169,7 +194,8 @@ public class HelloFX extends Application {
         platforms.add(plat6);
         platforms.add(plat7);
         
-        
+        int[] attempts = {1};
+
         
         
         
@@ -203,6 +229,7 @@ public class HelloFX extends Application {
                     if (player.getBoundsInParent().intersects(i.getBoundsInParent())) {
                         gameLoop.stop(); 
                         stage.setScene(deathScreen(stage));
+                        attempts[0]++;
                     }
                 }
 
@@ -213,6 +240,10 @@ public class HelloFX extends Application {
                         onGround[0] = true;
                     }
                 }
+
+                if (player.getX() == 8000) {
+
+                }
             }
         };
     
@@ -220,7 +251,7 @@ public class HelloFX extends Application {
         gameLoop.start();
         return scene;
         
-    }   
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -232,12 +263,6 @@ public class HelloFX extends Application {
         Scene menu = menuScreen(stage);
         stage.setScene(menu);
         stage.show();
-
-       
-
-        
-
-
 
     }
 
